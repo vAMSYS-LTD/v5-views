e3da46232e2d4e8a7f6090669f5a29f04e74c157
Lukas Jankauskas <as@lukasjankauskas.com>
2024-09-12 12:18:34 +0000

    Update default image logic and modify column in RankSettings
    Change default image URL handling in Rank model to always use placeholder image when HTTPS is detected. Adjust RankSettings to display the correct column name for images.

--- livewire/phoenix/dashboard/components/boxes.blade.php
+++ livewire/phoenix/dashboard/components/boxes.blade.php
@@ -27,7 +27,7 @@
             ],
         ];
 @endphp
-<div wire:key="dashboard.boxes" class="{{ $componentWidth }}" style="{{ (($firstBooking && $bookingCount >= 0) || (count($pireps) > 0)) ? '' : 'display: contents'}}">
+<div wire:key="dashboard.boxes" class="{{ $componentWidth }}" style="{{ (($firstBooking && $bookingCount >= 0) || ($pireps && (count($pireps) > 0)) ? '' : 'display: contents'}}">
     <div class="grid gap-2 {{ $widthCount[$block][$width] }}">
         @if($firstBooking && $bookingCount >= 0)
             <div wire:key="booking" wire:click="navigateBooking" class="card card-info dashboard-card-booking cursor-pointer">
