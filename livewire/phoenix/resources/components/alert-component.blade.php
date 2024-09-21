c966aa4dfd143c653260660327fb88422ca594b7
Lukas Jankauskas <as@lukasjankauskas.com>
2024-09-21 21:56:11 +0000

    Add !important to alert styles and fix class name typo
    This commit appends !important to various alert class styles to ensure higher CSS specificity. Additionally, it corrects a typo in the alert-component Blade file by adding the ProseMirror class to the alert types and fixes a misspelling of badge-warning in SCSS.

--- livewire/phoenix/resources/components/alert-component.blade.php
+++ livewire/phoenix/resources/components/alert-component.blade.php
@@ -2,22 +2,22 @@
     <div
         @switch($data['type'])
             @case('info')
-                class="alert alert-info"
+                class="alert alert-info ProseMirror"
         @break
         @case('success')
-            class="alert alert-success"
+            class="alert alert-success ProseMirror"
         @break
         @case('warning')
-            class="alert alert-warning"
+            class="alert alert-warning ProseMirror"
         @break
         @case('danger')
-            class="alert alert-error"
+            class="alert alert-error ProseMirror"
         @break
         @case('primary')
-            class="alert alert-primary"
+            class="alert alert-primary ProseMirror"
         @break
         @endswitch
     >
-        <span>{{ $content }}</span>
+        {{ $content }}
     </div>
 </div>
