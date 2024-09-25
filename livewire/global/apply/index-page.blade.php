5fe6d654dc9426d6e611c5a191134ec8ff4db589
Lukas Jankauskas <as@lukasjankauskas.com>
2024-09-25 16:16:28 +0000

    Fix manual jumpseat union code

---
+++ livewire/global/apply/index-page.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <div class="card">
+        <div class="card-header flex justify-between items-center">
+            <h4 class="card-title">Hello!</h4>
+        </div>
+        <div class="px-6 py-2 space-y-2">
+            <div>
+                We are thrilled to see you want to check out vAMSYS and perhaps start/transfer your Virtual Airline to our system.
+            </div>
+            <div>
+                We've just completed our migration to long-awaited v5 iteration and are unfortunately not quite ready for new Virtual Airlines just yet.
+            </div>
+            <div class="font-semibold">
+                Our documentation for new VAs is still work in progress, as is the new on-boarding process. Please check back tomorrow or day after - there's a high chance we will be accepting new Virtual Airlines then.
+            </div>
+        </div>
+    </div>
+</div>
