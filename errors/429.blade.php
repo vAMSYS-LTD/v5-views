2f73d28256d5047fa28ec1fd5a87638329e9cfb2
Lukas Jankauskas <as@lukasjankauskas.com>
2024-09-28 21:57:48 +0000

    Rate Limiter

--- errors/429.blade.php
+++ errors/429.blade.php
@@ -2,4 +2,13 @@
 
 @section('title', __('Too Many Requests'))
 @section('code', '429')
-@section('message', __('Too Many Requests'))
+
+@section('message')
+    <div class="text-sm">
+        Too many requests
+    </div>
+    <div class="text-sm">
+        You are making more than 100 requests per minute - check your browser and extensions.
+    </div>
+
+@endsection
