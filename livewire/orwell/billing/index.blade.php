77d58930a6a05dcf6517e3c4ab24940c02689bdd
Lukas Jankauskas <as@lukasjankauskas.com>
2024-09-28 20:15:33 +0000

    Initial help icons and links

--- livewire/orwell/billing/index.blade.php
+++ livewire/orwell/billing/index.blade.php
@@ -1,96 +1,74 @@
 <div>
     <div class="grid gap-6 grid-cols-3">
-        <div class="panel h-full">
-            <div class="-m-5 mb-5 flex items-start border-b border-[#e0e6ed] p-5 dark:border-[#1b2e4b]">
-                <div class="text-lg font-semibold">
-                    <h6>Why a recurring subscription?</h6>
-                </div>
+        <div class="card h-full">
+            <div class="card-header flex justify-between items-center">
+                <h4 class="card-title">Why a recurring subscription?</h4>
             </div>
-            <div>
-                <div class="text-justify text-white-dark">
-                    Our infrastructure - servers the vAMSYS runs on and the databases where we store all the delicious PIREPs does not come to us free of charge.<br/>
-                    Similar applies to all the other things we need to keep vAMSYS running - that's domains, development tools and some professional and legal fees.<br/>
-                    Our costs are ongoing and your subscription pays for it, allowing vAMSYS to remain online.
-                </div>
+            <div class="px-6 py-2 space-y-2">
+                Our infrastructure - servers the vAMSYS runs on and the databases where we store all the delicious PIREPs does not come to us free of charge.<br/>
+                Similar applies to all the other things we need to keep vAMSYS running - that's domains, development tools and some professional and legal fees.<br/>
+                Our costs are ongoing and your subscription pays for it, allowing vAMSYS to remain online.
             </div>
         </div>
-        <div class="panel h-full">
-            <div class="-m-5 mb-5 flex items-start border-b border-[#e0e6ed] p-5 dark:border-[#1b2e4b]">
-                <div class="text-lg font-semibold">
-                    <h6>How and when will I be charged?</h6>
-                </div>
+
+        <div class="card h-full">
+            <div class="card-header flex justify-between items-center">
+                <h4 class="card-title">How and when will I be charged?</h4>
             </div>
-            <div>
-                <div class="text-justify text-white-dark">
-                    @if(!$airline->subscribed())
-                        You currently do not have a subscription. Once you enter your card details, the billing
-                        period will begin immediately and your card will be charged monthly on the day subscription started or, when that day of the month does not exist - on the last day of the month. <br>
-                        If you are on trial period, it will end and your VA will be opened to registrations using the register link you have set in Settings.
+            <div class="px-6 py-2 space-y-2">
+                @if(!$airline->subscribed())
+                    You currently do not have a subscription. Once you enter your card details, the billing
+                    period will begin immediately and your card will be charged monthly on the day subscription started or, when that day of the month does not exist - on the last day of the month. <br>
+                    If you are on trial period, it will end and your VA will be opened to registrations using the register link you have set in Settings.
+                @else
+                    Your subscription started on {{ $airline->subscription()->created_at }} and will be billed
+                    @if($airline->subscribedToPrice('price_1I0UuFF9Ym5ZDQAIN8KpjFHE') || $airline->subscribedToPrice('price_1GyLNoF9Ym5ZDQAIcWcYqfMy'))
+                        every month
                     @else
-                        Your subscription started on {{ $airline->subscription()->created_at }} and will be billed
-                        @if($airline->subscribedToPrice('price_1I0UuFF9Ym5ZDQAIN8KpjFHE') || $airline->subscribedToPrice('price_1GyLNoF9Ym5ZDQAIcWcYqfMy'))
-                            every month
-                        @else
-                            every year
-                        @endif
-                        on the card provided on the day of the anniversary of the subscription or on the last day of the month.
+                        every year
                     @endif
-                </div>
-            </div>
-        </div>
-        <div class="panel h-full">
-            <div class="-m-5 mb-5 flex items-start border-b border-[#e0e6ed] p-5 dark:border-[#1b2e4b]">
-                <div class="text-lg font-semibold">
-                    <h6>Can I cancel?</h6>
-                </div>
-            </div>
-            <div>
-                <div class="text-justify text-white-dark">
-                    We are sorry to hear you are thinking of leaving - yes - you can cancel your subscription in the Manage Subscription page. Your VA will then be removed at the end of billing period.<br/>
-                    Should you wish the subscription to be terminated sooner, please get in touch with us by opening a ticket. Do note that no refund for the remaining period will be issued.
-                </div>
+                    on the card provided on the day of the anniversary of the subscription or on the last day of the month.
+                @endif
             </div>
         </div>
 
-        <div class="panel h-full">
-            <div class="-m-5 mb-5 flex items-start border-b border-[#e0e6ed] p-5 dark:border-[#1b2e4b]">
-                <div class="text-lg font-semibold">
-                    <h6>Are my card details safe?</h6>
-                </div>
+        <div class="card h-full">
+            <div class="card-header flex justify-between items-center">
+                <h4 class="card-title">Can I cancel?</h4>
             </div>
-            <div>
-                <div class="text-justify text-white-dark">
-                    Absolutely. <br/>
-                    Your card details do not enter or touch vAMSYS servers at all. All payment method storage and processing are done within your browser or by Stripe, our payment processor.<br/>
-                    <a href="/legal" target="_blank">See our Privacy Policy for more details.</a>
-                </div>
+            <div class="px-6 py-2 space-y-2">
+                We are sorry to hear you are thinking of leaving - yes - you can cancel your subscription in the Manage Subscription page. Your VA will then be removed at the end of billing period.<br/>
+                Should you wish the subscription to be terminated sooner, please get in touch with us by opening a ticket. Do note that no refund for the remaining period will be issued.
             </div>
         </div>
 
-        <div class="panel h-full">
-            <div class="-m-5 mb-5 flex items-start border-b border-[#e0e6ed] p-5 dark:border-[#1b2e4b]">
-                <div class="text-lg font-semibold">
-                    <h6>Can I change my plan from monthly to yearly?</h6>
-                </div>
+        <div class="card h-full">
+            <div class="card-header flex justify-between items-center">
+                <h4 class="card-title">Are my card details safe?</h4>
             </div>
-            <div>
-                <div class="text-justify text-white-dark">
-                    Yes - head to Manage Subscription page where you can make the change. Moving from monthly to yearly subscription will result in a pro-rated charge to extend your current billing period to a year, whereas change from yearly to monthly will give you credit and you will not be charged until it is exhausted.
-                </div>
+            <div class="px-6 py-2 space-y-2">
+                Absolutely. <br/>
+                Your card details do not enter or touch vAMSYS servers at all. All payment method storage and processing are done within your browser or by Stripe, our payment processor.<br/>
+                <a href="/legal" target="_blank">See our Privacy Policy for more details.</a>
             </div>
         </div>
 
-        <div class="panel h-full">
-            <div class="-m-5 mb-5 flex items-start border-b border-[#e0e6ed] p-5 dark:border-[#1b2e4b]">
-                <div class="text-lg font-semibold">
-                    <h6>What payment methods are accepted?</h6>
-                </div>
+        <div class="card h-full">
+            <div class="card-header flex justify-between items-center">
+                <h4 class="card-title">Can I change my plan from monthly to yearly?</h4>
             </div>
-            <div>
-                <div class="text-justify text-white-dark">
-                    All major credit and debit cards are accepted - this includes Visa, Mastercard, American Express, Discover & Diners and UnionPay.<br/>
-                    We politely ask you to use Visa or Mastercard where possible, due to more favorable card processing fees.
-                </div>
+            <div class="px-6 py-2 space-y-2">
+                Yes - head to Manage Subscription page where you can make the change. Moving from monthly to yearly subscription will result in a pro-rated charge to extend your current billing period to a year, whereas change from yearly to monthly will give you credit and you will not be charged until it is exhausted.
+            </div>
+        </div>
+
+        <div class="card h-full">
+            <div class="card-header flex justify-between items-center">
+                <h4 class="card-title">What payment methods are accepted?</h4>
+            </div>
+            <div class="px-6 py-2 space-y-2">
+                All major credit and debit cards are accepted - this includes Visa, Mastercard, American Express, Discover & Diners and UnionPay.<br/>
+                We politely ask you to use Visa or Mastercard where possible, due to more favorable card processing fees.
             </div>
         </div>
     </div>
