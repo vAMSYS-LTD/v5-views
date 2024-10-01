4207fca6999ddd1c7a60ba45b74883f316b44894
Lukas Jankauskas <as@lukasjankauskas.com>
2024-10-01 17:36:21 +0000

    Random Destination

--- livewire/phoenix/flight-center/book-flight-map.blade.php
+++ livewire/phoenix/flight-center/book-flight-map.blade.php
@@ -95,7 +95,28 @@
                         </p>
                     </div>
                 </div>
-                <div x-data="{ loading: false }" class="flex justify-around space-x-4 px-2 text-center">
+                <div id="data-buttons" x-data="{ loading: false }" class="flex justify-around space-x-2 px-2 text-center">
+                    <button
+                        id="random-destination-button"
+                        @click="loading = true; doRandomAirport().then(() => loading = false);"
+                        :class="{'opacity-50': loading}"
+                        :disabled="loading"
+                        type="button"
+                        class="btn btn-success w-full">
+                        <span x-show="!loading">Random Destination</span>
+                        <!-- An inline SVG spinner that is shown when loading -->
+                        <div class="flex items-center justify-center" x-show="loading">
+                            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                                 fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
+                            Loading...
+                        </div>
+                    </button>
+
                     <button
                         id="jumpseat-button"
                         @click="loading = true; doJumpseat().then(() => loading = false);"
@@ -138,6 +159,27 @@
                             Booking...
                         </div>
 
+                    </button>
+
+                    <button
+                        id="booking-self-button"
+                        @click="loading = true; doBookingSelf().then(() => loading = false);"
+                        :class="{'opacity-50': loading}"
+                        :disabled="loading"
+                        type="button"
+                        class="btn btn-success w-full" style="display: none">
+                        <span x-show="!loading">Book Flight</span>
+                        <!-- An inline SVG spinner that is shown when loading -->
+                        <div class="flex items-center justify-center" x-show="loading">
+                            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                                 fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
+                            Booking...
+                        </div>
                     </button>
                 </div>
             </div>
