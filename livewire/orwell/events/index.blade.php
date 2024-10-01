3a795e68f01477c1480c63b1ce5de74802d96198
Lukas Jankauskas <as@lukasjankauskas.com>
2024-10-01 16:15:08 +0000

    Do not Edit event notice

--- livewire/orwell/events/index.blade.php
+++ livewire/orwell/events/index.blade.php
@@ -1,3 +1,4 @@
 <div class="">
+    <x-alerts.danger title="Do Not Edit v3 Events" content="Editing a v3 event will break the event. If you have edited a v3 event and it now cannot be opened in Phoenix, please contact vAMSYS Support noting the event name and what type of event it is." />
     {{ $this->table }}
 </div>
