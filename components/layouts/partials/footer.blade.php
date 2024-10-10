733dae3af99047218c31093d96b5ad272047726a
Lukas Jankauskas <as@lukasjankauskas.com>
2024-10-10 15:02:20 +0000

    5.0.13

--- components/layouts/partials/footer.blade.php
+++ components/layouts/partials/footer.blade.php
@@ -9,7 +9,7 @@
                 Served By: {{ gethostname() }}
             </span>
             <a href="https://vamsys.dev" target="_blank" class="class=text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">
-                v5.0.12
+                v5.0.13
             </a>
 {{--            <a href="javascript: void(0);" class="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">Support</a>--}}
 {{--            <a href="javascript: void(0);" class="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">Contact Us</a>--}}
